255ae775c82e0dc28dea5edd923da560cf966b39
Jacob Tucker <tuckerjacob20@gmail.com>
2015-11-17 21:37:32 -0600

    Added in read

--- Decoder/DecoderMain.cpp
+++ Decoder/DecoderMain.cpp
@@ -1,22 +1,45 @@
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <sstream>
+
 struct Node {
 	Node *left;
 	Node *right;
-	int value;
+	std::string value;
 };
-Node* constructBST(int in[], int pre[], int n);
-int getIndex(int arr[], int size, int val);
+Node* constructBST(std::string in[], std::string pre[], int n);
+int getIndex(std::string arr[], int size, std::string val);
+std::vector<std::string> fillVector(std::vector<std::string>, std::string);
 
-int main() {
+int main(int argc, char* argv[]) {
+	if (argc != 2) {
+		std::cout << "Expected './main <filename>" << std::endl;
+		return 0;
+	}
+	std::ifstream treeInput(argv[1]);
+	std::string preorderTraversal = "";
+	std::string inorderTraversal = "";
 
-	int in[8] = { 4, 10, 3, 1, 7, 11, 8, 2 };
-	int pre[8] = { 7, 10, 4, 3, 1, 2, 8, 11 };
+	getline(treeInput, preorderTraversal);
+	getline(treeInput, inorderTraversal);
 
-	Node *tree = constructBST(in, pre, 8);
+	std::vector<std::string> inorder;
+	std::vector<std::string> preorder;
+
+	inorder = fillVector(inorder, inorderTraversal);
+	preorder = fillVector(preorder, preorderTraversal);
+	
+	std::string *in = &inorder[0];
+	std::string *pre = &preorder[0];
+
+	Node *tree = constructBST(in, pre, inorder.size());
 
 	return 0;
 }
 
-Node* constructBST(int in[], int pre[], int size) {
+Node* constructBST(std::string in[], std::string pre[], int size) {
 	if (size <= 0)
 		return nullptr;
 	int i = getIndex(in, size, pre[0]);
@@ -29,9 +52,18 @@ Node* constructBST(int in[], int pre[], int size) {
 	return temp;
 }
 
-int getIndex(int arr[], int size, int val) {
+int getIndex(std::string arr[], int size, std::string val) {
 	for (int i = 0; i < size; i++) {
 		if (arr[i] == val)
 			return i;
 	}
+}
+
+std::vector<std::string> fillVector(std::vector<std::string> v, std::string s) {
+	std::istringstream sstream(s);
+	std::string temp = "";
+	while (sstream >> temp) {
+		v.push_back(temp);
+	}
+	return v;
 }
